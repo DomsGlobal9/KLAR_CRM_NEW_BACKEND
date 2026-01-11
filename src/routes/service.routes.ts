5e537be592f8ac578a23f15c7cb19f3a114affbc
sainy2001 <135034424+sainy2001@users.noreply.github.com>
2026-01-11 23:02:33 +0530

    Modified in service route file

--- src/routes/service.routes.ts
+++ src/routes/service.routes.ts
@@ -7,11 +7,38 @@ const router = express.Router();
 router.get('/health', serviceController.healthCheck);
 
 // Service routes
+
+
+ /**
+  * API for create service
+  */
 router.post('/', serviceController.createService);
+
+/**
+ * API for get all services
+ */
 router.get('/', serviceController.getAllServices);
+
+
+/**
+ * API for get all services with relations
+ */
 router.get('/with-relations', serviceController.getAllServicesWithRelations);
+
+
+/**
+ * Api for get Services by ID
+ */
 router.get('/:id', serviceController.getServiceById);
+
+/**
+ * API for get service with relations
+ */
 router.get('/:id/with-relations', serviceController.getServiceWithRelations);
+
+/**
+ * API for put Update Service
+ */
 router.put('/:id', serviceController.updateService);
 router.delete('/:id', serviceController.deleteService);
 router.patch('/:id/toggle-status', serviceController.toggleServiceStatus);
