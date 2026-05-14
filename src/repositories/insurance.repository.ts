a7fcecc988f60897f38ffb0bfe229e81979f16c2
vaishnavi9908 <vaishnavi.developer@domsglobal.co>
2026-05-14 21:59:56 +0530

    updates are added

--- src/repositories/insurance.repository.ts
+++ src/repositories/insurance.repository.ts
@@ -1,10 +1,29 @@
-import { InsuranceBookingModel } from "../models/insurance-booking.model";
+// import { InsuranceBookingModel } from "../models/insurance-booking.model";
 
 
-export const findAllBookings = async () => {
-    return await InsuranceBookingModel.find().lean();
+// export const findAllBookings = async () => {
+//     return await InsuranceBookingModel.find().lean();
+// };
+
+// export const findBookingById = async (bookingId: string) => {
+//     return await InsuranceBookingModel.findOne({ bookingId }).lean();
+// };
+
+
+
+
+
+
+
+
+
+
+import { InsuranceBookingModel, IInsuranceBooking } from "../models/insurance.model";
+
+export const findInsuranceBookings = async (query: any = {}) => {
+    return await InsuranceBookingModel.find(query).lean();
 };
 
-export const findBookingById = async (bookingId: string) => {
+export const findInsuranceBookingById = async (bookingId: string) => {
     return await InsuranceBookingModel.findOne({ bookingId }).lean();
 };
