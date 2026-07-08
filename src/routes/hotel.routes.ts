b815a53f57640aa97c874220d5d505a91275da3a
vaishnavi9908 <vaishnavi.developer@domsglobal.co>
2026-07-08 19:29:41 +0530

    comented code is removed

--- src/routes/hotel.routes.ts
+++ src/routes/hotel.routes.ts
@@ -1,34 +1,3 @@
-// import { Router } from "express";
-// import { getHotelReport, getSingleHotelBooking } from "../controllers/hotel.controller";
-// import { authenticate } from '../middleware/auth.middleware';
-
-// const router = Router();
-
-// // List report (summary fields)
-// router.get("/hotel-report", authenticate, getHotelReport);
-
-// // Full details (single booking)
-// router.get("/hotel-report/:reservationId", authenticate, getSingleHotelBooking);
-
-// export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { Router } from "express";
 import { 
     getHotelReport, 
