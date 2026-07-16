d9cf5eef170aca0cd57e6a0088edf8ced6717339
DomsGlobal9 <domsgloballlp@gmail.com>
2026-07-16 17:29:35 +0530

    Fix: Drizzle issue fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,11 +4,13 @@ import { envConfig } from './src/config/env.config';
 const isProduction = envConfig.NODE_ENV === "production";
 let supabaseUrl = '';
 
+console.log("[DEBUG] Is Production", isProduction);
+
 if (isProduction) {
-  supabaseUrl = envConfig.SUPABASE_DATABASE_URL
+  supabaseUrl = envConfig.SUPABASE_PRODUCTION_DATABASE_URL
 }
 else {
-  supabaseUrl = envConfig.SUPABASE_PRODUCTION_DATABASE_URL
+  supabaseUrl = envConfig.SUPABASE_DATABASE_URL
 }
 
 export default defineConfig({
